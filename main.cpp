d8904d2343bebac7e7a7afd056b898a19df321ee
Alice (Nikita K.V.) <konopelkonikita894@gmail.com>
2024-07-07 19:18:17 +0300

    ...

--- main.cpp
+++ main.cpp
@@ -7,9 +7,11 @@ int main()
     cout << "Enter A and B: ";
     int a, b;
     cin >> a >> b;
+    int max = a > b ? a : b;
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n'
          << "A * B = " << a * b << '\n'
-         << "A / B = " << a / b << '\n';
+         << "A / B = " << a / b << '\n'
+         << "MAX = " << max << '\n';
     return 0;
 }
